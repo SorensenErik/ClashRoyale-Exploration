bb618e70d0d6b4cb38eaecf5b5219031ebece893
SorensenErik <sorensen.erik48@gmail.com>
2019-04-14 00:47:39 -0400

    Fixed a bug where win percentage showing up incorrectly

--- playerMatchAnalysis.py
+++ playerMatchAnalysis.py
@@ -13,11 +13,15 @@ os.chdir("C:/Github/ClashRoyale-Exploration") # For Atom Hydrogen
 # ladder_games = glob.glob("datadumps/playerData/*Ladder.json")
 # ladder_games = [x.replace("\\","/") for x in ladder_games]
 # ladder_games
-for filename in os.listdir("datadumps/playerData"):
-    with open('datadumps/playerData/{:}'.format(filename), 'r') as f:
-        data = json.load(f) # This is all of the data in the folder
+for filename in os.listdir("datadumps/playerData/CrypticSl0th_98Y2CVV8"):
+    with open('datadumps/playerData/CrypticSl0th_98Y2CVV8/{:}'.format(filename), 'r') as f:
+        Cryp_data = json.load(f) # This is all of the data in the folder
 
-def ladder_summary():
+for filename in os.listdir("datadumps/playerData/Jinakaks_8020JC98P"):
+    with open('datadumps/playerData/Jinakaks_8020JC98P/{:}'.format(filename), 'r') as f:
+        Jin_data = json.load(f) # This is all of the data in the folder
+
+def ladder_summary(data):
     ladder_games = []
     for match in data:
         if match['mode']['name'] == 'Ladder':
@@ -25,7 +29,12 @@ def ladder_summary():
 
     wins = []
     for match in ladder_games:
-        wins.append(match['winner'])
+        win = match['winner']
+        if win == -1: # Correct for match['winner'] saying either 2 or 3 instead of 1 for wins
+            wins.append(win)
+        else:
+            wins.append(1)
+    print(wins)
     win_loss_ratio = wins.count(1)/len(wins)
 
     trophy_change = []
@@ -37,5 +46,8 @@ def ladder_summary():
     print("For the last {:} ladder games:".format(len(ladder_games)))
     print("You have a {:.4f}% win rate.".format(win_loss_ratio))
     print("You have gained an average of {:.4f} trophies per game.".format(avg_trophy_gain))
+    print(wins)
+    print(trophy_change)
 
-ladder_summary()
+ladder_summary(Cryp_data)
+ladder_summary(Jin_data)
