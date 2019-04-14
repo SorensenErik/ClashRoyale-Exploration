595fbb206171b0c24f1544d3e40680a76e843419
SorensenErik <sorensen.erik48@gmail.com>
2019-04-14 00:48:48 -0400

    Added functionality for tracking different players

--- playerDataPullScript.py
+++ playerDataPullScript.py
@@ -1,15 +1,21 @@
 import json
 import os
-os.chdir("C:/GitHub/ClashRoyale-Exploration") # For Hydrogen Atom
+#os.chdir("C:/GitHub/ClashRoyale-Exploration") # For Hydrogen Atom
 from apiaccess import make_request
 
-data = make_request("https://api.royaleapi.com/player/98Y2CVV8/battles")
+data_98Y2CVV8 = make_request("https://api.royaleapi.com/player/98Y2CVV8/battles")
+data_8020JC98P = make_request("https://api.royaleapi.com/player/8020JC98P/battles")
 
-def save_data(data):
+def save_player_data(data,dirname):
+    # Make dir if not exists
+    if not os.path.exists('datadumps/playerData/{:}'.format(dirname)):
+        os.makedirs('datadumps/playerData/{:}'.format(dirname))
+    # Write the data in that directory
     for match in data:
         game_mode = match['mode']['name']
         utcTime = match['utcTime']
-        with open('datadumps/playerData/{:}-{:}.json'.format(utcTime,game_mode),'w') as outfile:
+        with open('datadumps/playerData/{:}/{:}-{:}.json'.format(dirname,utcTime,game_mode),'w') as outfile:
             json.dump(data, outfile)
 
-save_data(data)
+save_player_data(data_98Y2CVV8,"CrypticSl0th_98Y2CVV8")
+save_player_data(data_8020JC98P,"Jinakaks_8020JC98P")
