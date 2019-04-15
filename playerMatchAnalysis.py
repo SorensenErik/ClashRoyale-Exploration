5c6fb1e5c5dbe2985bb98fcd64e7a843602b3775
SorensenErik <sorensen.erik48@gmail.com>
2019-04-15 19:05:39 -0400

    trophyCount no longer available with new April update

--- playerMatchAnalysis.py
+++ playerMatchAnalysis.py
@@ -13,13 +13,15 @@ os.chdir("C:/Github/ClashRoyale-Exploration") # For Atom Hydrogen
 # ladder_games = glob.glob("datadumps/playerData/*Ladder.json")
 # ladder_games = [x.replace("\\","/") for x in ladder_games]
 # ladder_games
+Cryp_data = []
 for filename in os.listdir("datadumps/playerData/CrypticSl0th_98Y2CVV8"):
     with open('datadumps/playerData/CrypticSl0th_98Y2CVV8/{:}'.format(filename), 'r') as f:
-        Cryp_data = json.load(f) # This is all of the data in the folder
+        Cryp_data.append(json.load(f)) # This is all of the data in the folder
 
+Jin_data = []
 for filename in os.listdir("datadumps/playerData/Jinakaks_8020JC98P"):
     with open('datadumps/playerData/Jinakaks_8020JC98P/{:}'.format(filename), 'r') as f:
-        Jin_data = json.load(f) # This is all of the data in the folder
+        Jin_data.append(json.load(f)) # This is all of the data in the folder
 
 def filter_games(data,game_type):
     # games = []
@@ -39,21 +41,23 @@ def extract_wins(data):
             yield 1
     # return wins
 
+Cryp_data[0]
 def ladder_summary(data):
     ladder_games = list(filter_games(data,'Ladder'))
 
     wins = list(extract_wins(data))
     win_loss_ratio = wins.count(1)/len(wins)
 
-    trophy_change = []
-    for match in ladder_games:
-        trophy_change.append(match['team'][0]['trophyChange'])
-    avg_trophy_movement = np.mean(np.abs(trophy_change))
-    avg_trophy_gain = np.mean(trophy_change)
+    # No longer have 'trophyChange' data for each match
+    # trophy_change = []
+    # for match in ladder_games:
+    #     trophy_change.append(match['team'][0]['trophyChange'])
+    # avg_trophy_movement = np.mean(np.abs(trophy_change))
+    # avg_trophy_gain = np.mean(trophy_change)
 
     print("For the last {:} ladder games:".format(len(list(ladder_games))))
     print("You have a {:.4f}% win rate.".format(win_loss_ratio))
-    print("You have gained an average of {:.4f} trophies per game.".format(avg_trophy_gain))
+    # print("You have gained an average of {:.4f} trophies per game.".format(avg_trophy_gain))
 
 ladder_summary(Cryp_data)
 ladder_summary(Jin_data)
