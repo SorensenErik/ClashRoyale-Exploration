dfaf69fe4d6416071af63d309dddbc6a6f6fe52f
SorensenErik <sorensen.erik48@gmail.com>
2019-04-23 16:21:50 -0400

    Changed datadump path to larger disk

--- playerDataPullScript.py
+++ playerDataPullScript.py
@@ -3,19 +3,20 @@ import os
 #os.chdir("C:/GitHub/ClashRoyale-Exploration") # For Hydrogen Atom
 from apiaccess import make_request
 
+path = 'D:/DataSets/ClashRoyale/playerData/'
 data_98Y2CVV8 = make_request("https://api.royaleapi.com/player/98Y2CVV8/battles")
 data_8020JC98P = make_request("https://api.royaleapi.com/player/8020JC98P/battles")
 
-def save_player_data(data,dirname):
+def save_player_data(path,data,dirname):
     # Make dir if not exists
-    if not os.path.exists('datadumps/playerData/{:}'.format(dirname)):
-        os.makedirs('datadumps/playerData/{:}'.format(dirname))
+    if not os.path.exists('{:}{:}'.format(path,dirname)):
+        os.makedirs('{:}{:}'.format(path,dirname))
     # Write the data in that directory
     for match in data:
         game_mode = match['mode']['name']
         utcTime = match['utcTime']
-        with open('datadumps/playerData/{:}/{:}-{:}.json'.format(dirname,utcTime,game_mode),'w') as outfile:
+        with open('{:}{:}/{:}-{:}.json'.format(path,dirname,utcTime,game_mode),'w') as outfile:
             json.dump(match, outfile)
 
-save_player_data(data_98Y2CVV8,"CrypticSl0th_98Y2CVV8")
-save_player_data(data_8020JC98P,"Jinakaks_8020JC98P")
+save_player_data(path,data_98Y2CVV8,"CrypticSl0th_98Y2CVV8")
+save_player_data(path,data_8020JC98P,"Jinakaks_8020JC98P")
